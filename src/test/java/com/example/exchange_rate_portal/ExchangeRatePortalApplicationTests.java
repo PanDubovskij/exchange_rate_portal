7a98731af159acff6a1c79856792fb1191e8253c
JanDubovsky <jan.dubovsky@specific-group.com>
2024-04-25 19:51:07 +0300

    init

---
+++ src/test/java/com/example/exchange_rate_portal/ExchangeRatePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchange_rate_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
